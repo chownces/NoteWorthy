376b20c80306d439003ee0e5fc09790f056cc7a3
Chow En Rong <53928333+chownces@users.noreply.github.com>
2021-05-11 21:28:09 +0800

    Implemented 404 page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,5 +1,15 @@
+import { Header, Icon } from 'semantic-ui-react';
+
 const NotFound: React.FC = () => {
-  return <div>404 page</div>;
+  return (
+    <Header as="h1" className="notfound-container">
+      <div className="notfound-icon-group">
+        <Icon name="frown outline" size="big" />
+        <div className="notfound-header">404 Page not found</div>
+      </div>
+      <Header.Subheader>Alas, this note was not worthy...</Header.Subheader>
+    </Header>
+  );
 };
 
 export default NotFound;
