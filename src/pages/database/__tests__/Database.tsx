34db339cd34f868c79fdc659fb2b90ac64a8f1e7
aikenwx <59946240+aikenwx@users.noreply.github.com>
2021-06-02 16:38:21 +0800

    Fixed snapshot test.

--- src/pages/database/__tests__/Database.tsx
+++ src/pages/database/__tests__/Database.tsx
@@ -25,18 +25,32 @@ test('Gets all notes successfully and renders them correctly', async () => {
             currentView: 'table',
             notes: [
               {
+                id: '60adcd6e38a95f0f75f1c089',
                 userId: '60adcd0138a95f0f75f1c085',
                 databaseId: '60adcd3538a95f0f75f1c087',
-                id: '60adcd6e38a95f0f75f1c089',
                 title: 'My first note',
+                blocks: [
+                  {
+                    id: 'kpedjle3u7crjgm0wn',
+                    html: '',
+                    tag: 'p'
+                  }
+                ],
                 creationDate: '2021-05-26T04:21:57.091Z',
                 latestUpdate: '2021-05-26T08:09:57.274Z'
               },
               {
+                id: '6098bbdc04c0fa63070bfe5e',
                 userId: '6098bbdc04c0fa63070bfe4e',
                 databaseId: '6098ba47fdaba558e1955a23',
-                id: '6098bbdc04c0fa63070bfe5e',
                 title: 'untitled',
+                blocks: [
+                  {
+                    id: 'kpedjmkovw5eohfiu6',
+                    html: '',
+                    tag: 'p'
+                  }
+                ],
                 creationDate: '2021-05-26T08:09:57.274Z',
                 latestUpdate: '2021-05-26T08:09:57.274Z'
               }
@@ -67,7 +81,7 @@ test('Renders loading state correctly', async () => {
       request: {
         query: GET_DATABASE_QUERY
       },
-      variables: { id: '60adcd3538a95f0f75f1c087' },
+      variables: { id: '6098ba47fdaba558e1955a23' },
       result: {
         data: {
           getDatabase: {
@@ -76,18 +90,32 @@ test('Renders loading state correctly', async () => {
             currentView: 'table',
             notes: [
               {
+                id: '6098ba47fdaba558e1955a00',
                 userId: '6098ba47fdaba558e1955a12',
                 databaseId: '6098ba47fdaba558e1955a23',
-                id: '6098ba47fdaba558e1955a00',
                 title: 'test note 1',
+                blocks: [
+                  {
+                    id: 'kpedjmkovw5eohfiu6',
+                    html: '',
+                    tag: 'p'
+                  }
+                ],
                 creationDate: '2021-05-26T08:09:57.274Z',
                 latestUpdate: '2021-05-26T08:12:57.274Z'
               },
               {
+                id: '6098bbdc04c0fa63070bfe5e',
                 userId: '6098bbdc04c0fa63070bfe4e',
                 databaseId: '6098ba47fdaba558e1955a23',
-                id: '6098bbdc04c0fa63070bfe5e',
                 title: 'untitled',
+                blocks: [
+                  {
+                    id: 'kpedjn9mc0yez9tep1a',
+                    html: '',
+                    tag: 'p'
+                  }
+                ],
                 creationDate: '2021-05-26T08:09:57.274Z',
                 latestUpdate: '2021-05-26T08:09:57.274Z'
               }
